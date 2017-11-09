1dff96ca73974b880e61c9a6413cfbcd42d05c04
Tit <titpost@yandex.ru>
2017-11-09 21:24:24 +0300

    index.html is finally mapped!

--- src/main/java/com/epam/lab/payments/IndexController.java
+++ src/main/java/com/epam/lab/payments/IndexController.java
@@ -2,23 +2,30 @@ package com.epam.lab.payments;
 
 import com.epam.lab.payments.dao.CreditCardRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Controller
-@RequestMapping("/")
 public class IndexController {
-
+/*
     //@GetMapping("/")
+    @RequestMapping(value = "/")
     public ModelAndView index() {
         Map<String, String> model = new HashMap<>();
         model.put("name", "Cards:");
 
-
         return new ModelAndView("index", model);
+    }*/
+
+    @RequestMapping("/")
+    public String index() {
+        return "index.html";
     }
+
 }
